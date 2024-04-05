a66aeac9cdb99be39992b42664221ec63bda4a25
vishalkumar416 <vishal2221416@gmail.com>
2024-04-06 00:58:32 +0530

    New changes

---
+++ main.js
@@ -0,0 +1,16 @@
+Array.from(document.getElementsByTagName('input')).forEach((e,i)=>{
+  e.addEventListener('keyup',(e1)=>{
+    if(e.value.length > 0){
+      document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
+    }
+    else{
+      document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
+    }
+  })
+})
+let menu_btn = document.getElementsByClassName('bi-three-dots')[0];
+let menu_box=document.getElementById('menu_box');
+
+menu_btn.addEventListener('click',()=>{
+  menu_box.classList.toggle('ul_active');
+})
